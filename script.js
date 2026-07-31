38f5775e170a8602345b47eab31ebcd991b1e530
kaho99 <kahoefootbal@gmail.com>
2026-07-31 03:32:14 +0000

    Revert "Remove relevant device matches section from generated report"
    This reverts commit 80d67531352e237b2c1de44bc88680fd6a8f9de0.

--- script.js
+++ script.js
@@ -363,7 +363,7 @@ function parseReportFields(formData) {
   };
 }
 
-function generateReportForm(formData, reportFields) {
+function generateReportForm(formData, reportFields, matchHtml) {
   const sourceText = escapeHtml(reportFields.sourceText);
   const issuingAuthority = escapeHtml(reportFields.issuingAuthority);
   const alertCategory = escapeHtml(reportFields.alertCategory);
@@ -415,6 +415,10 @@ function generateReportForm(formData, reportFields) {
           </tr>
         </tbody>
       </table>
+
+      <br/><br/>
+      <div class="report-row report-label">Relevant device matches:</div>
+      ${matchHtml}
 
       <br/><br/>
       <div class="report-row report-underline">prepared by:</div>
@@ -536,7 +540,9 @@ generateReportButton.addEventListener('click', () => {
   };
 
   const reportFields = parseReportFields(formData);
-  resultTable.innerHTML = generateReportForm(formData, reportFields);
+  const result = screenAlert(formData.alertText, reportFields.sourceText, formData.link, formData.serialPart);
+  const matchHtml = buildTable(result.matches);
+  resultTable.innerHTML = generateReportForm(formData, reportFields, matchHtml);
   resultSummary.innerHTML = '<strong>Report form generated.</strong> Use Export as PDF to print the visible report.';
 });
 
